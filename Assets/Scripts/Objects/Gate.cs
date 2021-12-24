b85e3eba3ffeef1632364ba3228088b1440892c7
Alper Kılınç <akilinc17@ku.edu.tr>
2021-12-24 13:32:41 +0300

    Add star particle to gate.

--- Assets/Scripts/Objects/Gate.cs
+++ Assets/Scripts/Objects/Gate.cs
@@ -5,12 +5,28 @@ using UnityEngine;
 
 public class Gate : MonoBehaviour
 {
-    
+    [SerializeField] private ParticleSystem starParticle;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("EasterEgg"))
         {
             other.GetComponent<EasterEggBehaviour>().ActivateRibbon();
+            
+            var starParticlePosition = starParticle.transform.position;
+            starParticlePosition.x = other.transform.position.x;
+            starParticle.transform.position = starParticlePosition;
+
+            starParticle.Play();
         }
+    }
+    
+    private void ChangeMaterial(Collider other)
+    {
+        var starParticlePosition = starParticle.transform.position;
+        starParticlePosition.x = other.transform.position.x;
+        starParticle.transform.position = starParticlePosition;
+
+        starParticle.Play();
     }
 }
