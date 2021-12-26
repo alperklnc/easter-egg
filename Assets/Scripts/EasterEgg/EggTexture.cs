6a4e05d19461c5e203ab4c6d825ad086a6499b00
Alper Kılınç <akilinc17@ku.edu.tr>
2021-12-26 13:05:09 +0300

    Add new egg textures.

--- Assets/Scripts/EasterEgg/EggTexture.cs
+++ Assets/Scripts/EasterEgg/EggTexture.cs
@@ -13,34 +13,6 @@ namespace DefaultNamespace
         Purple
     }
 
-    public static class ChocolateColor
-    {
-        
-        static Color Bronze = new Color(0.66f, 0.22f, 0.2f);
-        static Color Gold = new Color(0.81f, 0.65f, 0.3f);
-        static Color Pink = new Color(0.8f, 0.2f, 0.4f);
-        static Color Purple = new Color(0.66f, 0.24f, 0.67f);
-
-        public static Color GetChocolateColor(Chocolate chocolate)
-        {
-            switch (chocolate)
-            {
-                case Chocolate.None:
-                    return Color.white;
-                case Chocolate.Bronze:
-                    return Bronze;
-                case Chocolate.Gold:
-                    return Gold;
-                case Chocolate.Pink:
-                    return Pink;
-                case Chocolate.Purple:
-                    return Purple;
-                default:
-                    return Color.white;
-            }
-        }
-    }
-
     public enum Pattern
     {
         Empty,
@@ -53,6 +25,9 @@ namespace DefaultNamespace
         Zigzag2,
         Zigzag3,
         Zigzag4,
-        
+        Pattern1,
+        Pattern2,
+        Pattern3,
+        Pattern4,
     }
 }
